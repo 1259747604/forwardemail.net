1284a35d7ad8a6b5142300fbbe6d0e26276e7827
tt <teng.tang@ly.com>
2024-03-11 21:38:41 +0800

    feat: 配置修改

--- test1.js
+++ test1.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
     res.end('Hello, world!');
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+server.listen(3003, 'localhost', () => {
+    console.log('Server running at http://localhost:3003/');
 });
